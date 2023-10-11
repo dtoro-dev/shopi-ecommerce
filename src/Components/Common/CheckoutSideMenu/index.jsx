12137d64cd2f65f3a3a0c922c7142b703f8829ec
Diego Toro <dtoro.a.s@gmail.com>
2023-10-11 18:50:14 -0300

    checkout order

--- src/Components/Common/CheckoutSideMenu/index.jsx
+++ src/Components/Common/CheckoutSideMenu/index.jsx
@@ -1,5 +1,6 @@
 import { XMarkIcon } from "@heroicons/react/24/solid";
-import { useContext, useEffect, useRef } from "react";
+import moment from "moment";
+import { useContext, useEffect, useRef, useState } from "react";
 import { CartContext } from "../../../Context/CartContext";
 import { priceFormat, totalPrice, totalTax } from "../../../utils";
 import OrderCard from "../OrderCart";
@@ -8,10 +9,19 @@ import "./styles.css";
 const CheckoutSideMenu = () => {
   const context = useContext(CartContext);
   const containerCartRef = useRef();
+  const [receipt, setReceipt] = useState({
+    totalPrice: 0,
+    totalTax: 0,
+    total: 0,
+  });
 
-  useEffect(() => {
-    containerCartRef.current.scrollTop = containerCartRef.current.scrollHeight;
-  }, [context.cartProduct]);
+  const calculateReceipt = () => {
+    setReceipt({
+      totalPrice: totalPrice(context.cartProduct),
+      totalTax: totalTax(context.cartProduct),
+      total: totalPrice(context.cartProduct) + totalTax(context.cartProduct),
+    });
+  };
 
   const hideCheckout = () => {
     context.hideCheckoutSideMenu();
@@ -22,6 +32,24 @@ const CheckoutSideMenu = () => {
 
     context.setCartProduct(newCartProduct);
   };
+
+  const handleCheckout = () => {
+    const newOrder = {
+      date: moment().format("DD/MM/YYYY HH:mm:ss"),
+      products: context.cartProduct,
+      totalProducts: context.cartProduct?.length || 0,
+      ...receipt,
+    };
+
+    context.setOrder([...context.order, newOrder]);
+
+    context.setCartProduct([]);
+  };
+
+  useEffect(() => {
+    calculateReceipt();
+    containerCartRef.current.scrollTop = containerCartRef.current.scrollHeight;
+  }, [context.cartProduct]);
 
   return (
     <aside
@@ -40,7 +68,10 @@ const CheckoutSideMenu = () => {
           <XMarkIcon className="h-5 w-5" />
         </div>
       </div>
-      <div className="overflow-y-auto p-3 h-4/6 bg-white/40" ref={containerCartRef}>
+      <div
+        className="overflow-y-auto p-3 h-4/6 bg-white/40"
+        ref={containerCartRef}
+      >
         {context.cartProduct?.map((product, index) => (
           <OrderCard
             key={index}
@@ -52,26 +83,26 @@ const CheckoutSideMenu = () => {
       <div className="w-full p-3 font-bold flex flex-col gap-2">
         <div className="flex justify-between">
           <p>TOTAL NETO</p>
-          <p className="font-medium text-2xl">$ {priceFormat(totalPrice(context.cartProduct))}</p>
+          <p className="font-medium text-2xl">
+            $ {priceFormat(receipt.totalPrice)}
+          </p>
         </div>
         <div className="flex justify-between">
           <p>IVA (19%)</p>
-          <p className="font-medium text-2xl">$ {priceFormat(totalTax(context.cartProduct))}</p>
+          <p className="font-medium text-2xl">
+            $ {priceFormat(receipt.totalTax)}
+          </p>
         </div>
         <div className="flex justify-between">
           <p>TOTAL</p>
-          <p className="font-medium text-2xl">
-            ${" "}
-            {priceFormat(
-              totalPrice(context.cartProduct) + totalTax(context.cartProduct)
-            )}
-          </p>
+          <p className="font-medium text-2xl">$ {priceFormat(receipt.total)}</p>
         </div>
         <button
-          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
+          className={`w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded ${
             totalPrice(context.cartProduct) === 0 ? "disabled:bg-sky-950" : ""
           }`}
           disabled={totalPrice(context.cartProduct) === 0}
+          onClick={() => handleCheckout()}
         >
           PAGAR
         </button>
